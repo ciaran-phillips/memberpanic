c442507047c315ea8551459b25851e64c710b742
Ciaran Phillips <ciaranphillips1@gmail.com>
2013-07-12 21:27:04 +0100

    Style charts as separate widgets

--- html/js/simpleweather.js
+++ html/js/simpleweather.js
@@ -6,10 +6,10 @@ $(document).ready(function() {
     location: '',
     unit: 'c',
     success: function(weather) {
-      html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      
+      html = '<ul><li>'+weather.city+', '+weather.region+'</li>';
       html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<li>'+weather.tempAlt+'&deg;C</li></ul>';
+      html += '<li>'+weather.temp+'&deg;C</li></ul>';
   
       $("#weather").html(html);
     },
